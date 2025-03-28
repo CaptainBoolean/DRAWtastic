eaee9255862b8c48bf722e8d4a855e364ea6338c
Julian Aigelsdorfer <julian.aigelsdorfer@student.tugraz.at>
2025-03-28 18:54:32 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example.multimediagroupproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens org.example.multimediagroupproject to javafx.fxml;
+    exports org.example.multimediagroupproject;
+}
