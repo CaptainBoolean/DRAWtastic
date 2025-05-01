50cf1957678a27af206a1d88067b0f11ec6b40ff
Paul Moor <paul.alexander.moor@gmail.com>
2025-05-01 19:59:01 +0200

    try solve merge fail

--- src/main/java/org/example/paint/tools/textField/TextColor.java
+++ src/main/java/org/example/paint/tools/textField/TextColor.java
@@ -1,6 +1,5 @@
 package org.example.paint.tools.textField;
 
 public enum TextColor {
-    //TODO maybe take text color from the selected pen color
     BLACK, RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, WHITE
 }
