154aa4d48b763e7dae46ef5d33dc5428698d0721
schreibtisch1-new-new <g.thieving466@passinbox.com>
2025-05-03 11:23:38 +0200

    Started working on Pipette: testing it; maybe works

--- src/main/java/org/example/paint/tools/generalTools/Pipette.java
+++ src/main/java/org/example/paint/tools/generalTools/Pipette.java
@@ -2,6 +2,7 @@ package org.example.paint.tools.generalTools;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import org.example.paint.tools.Tool;
@@ -16,10 +17,21 @@ public class Pipette implements Tool {
   }
 
   //TODO implement - leave constructor and just modify the color.setValue with the color of the onRelease pixel
+  //Done
 
   @Override
   public void onRelease(GraphicsContext g, MouseEvent e, double size) {
+    double x = e.getX();
+    double y = e.getY();
 
+    WritableImage image = new WritableImage(1,1);
+    g.getCanvas().snapshot(null, image);
+
+    Color pickedColor = image.getPixelReader().getColor((int) x, (int) y);
+
+    if (color != null) {
+      color.setValue(pickedColor);
+    }
   }
 
 }
