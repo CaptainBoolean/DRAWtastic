9dd29c4bf7b5da2c2a080a17e382ee65bd8603b3
kaoshtern <jufa2000@gmx.at>
2025-05-08 20:45:19 +0200

    Rectangle bisl verbessert

--- src/main/java/org/example/paint/tools/shapes/Rectangle.java
+++ src/main/java/org/example/paint/tools/shapes/Rectangle.java
@@ -6,36 +6,72 @@ import javafx.scene.paint.Color;
 
 public class Rectangle extends Shape {
     private double startX, startY;
-    private final Color color;
-
-    public Rectangle() {
-        this.color = Color.BLACK;
-    }
+    private double lastPreviewX = -1;
+    private double lastPreviewY = -1;
+    protected double maxSizeFactor = 4;
+    private boolean drawing = false;
+    private Color currColor = Color.BLACK;
+    private double currOpacity = 1;
 
     @Override
     public void onPress(GraphicsContext drawShape, MouseEvent mouse) {
         startX = mouse.getX();
         startY = mouse.getY();
+        drawing = true;
     }
 
     @Override
     public void onDrag(GraphicsContext drawShape, MouseEvent mouse, double size, Color color, double opacity) {
+        if (!drawing) {
+            return;
+        }
+        currColor = color;
+        currOpacity = opacity;
+
+        if (lastPreviewX != -1 && lastPreviewY != -1) {
+            double saveSize = size * maxSizeFactor;
+            double clearX = Math.min(startX, lastPreviewX);
+            double clearY = Math.min(startY, lastPreviewY);
+            double clearWidth = Math.abs(lastPreviewX - startX);
+            double clearHeight = Math.abs(lastPreviewY - startY);
+            drawShape.clearRect(clearX - saveSize / 2, clearY - saveSize / 2, clearWidth + saveSize, clearHeight + saveSize);
+        }
+
+        double endX = mouse.getX();
+        double endY = mouse.getY();
+        drawShape.setGlobalAlpha(opacity);
+        drawShape.setStroke(color);
+        drawShape.setLineWidth(size);
+        drawShape.strokeRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+        drawShape.setGlobalAlpha(1.0);
+
+        lastPreviewX = endX;
+        lastPreviewY = endY;
     }
 
     @Override
     public void onRelease(GraphicsContext drawShape, MouseEvent mouse, double size) {
-        double endX = mouse.getX();
-        double endY = mouse.getY();
+        if (drawing) {
+            double endX = mouse.getX();
+            double endY = mouse.getY();
 
-        drawShape.setStroke(color);
-        drawShape.setLineWidth(size);
-        drawShape.strokeRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+            drawShape.setGlobalAlpha(currOpacity);
+            drawShape.setStroke(currColor);
+            drawShape.setLineWidth(size);
+            drawShape.strokeRect(Math.min(startX, endX), Math.min(startY, endY), Math.abs(endX - startX), Math.abs(endY - startY));
+            drawShape.setGlobalAlpha(1);
+
+            lastPreviewX = -1;
+            lastPreviewY = -1;
+            drawing = false;
+        }
     }
 
     @Override
-    public void drawPreviewAt(GraphicsContext og, MouseEvent e, double size) {
-        //TODO implement (for inspiration see e.g. Pen)
+    public void drawPreviewAt(GraphicsContext drawShape, MouseEvent mouse, double size) {
+        if (drawing) {
+            onDrag(drawShape, mouse, size, currColor, currOpacity);
+        }
     }
-
 
 }
