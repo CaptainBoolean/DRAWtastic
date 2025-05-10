7b75acc4cf005b191d6f2e44f3f8e9f2ad06b9fc
schreibtisch1-new-new <g.thieving466@passinbox.com>
2025-05-10 11:14:40 +0200

    PaintBucket.java: Structure of PaintBucket is done; only method floodFill has to be implemented

--- src/main/java/org/example/paint/tools/generalTools/PaintBucket.java
+++ src/main/java/org/example/paint/tools/generalTools/PaintBucket.java
@@ -1,7 +1,42 @@
 package org.example.paint.tools.generalTools;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import org.example.paint.tools.Tool;
 
 public class PaintBucket implements Tool {
-  //TODO implement
+
+  private ObjectProperty<Color> fillColor = null;
+
+  public PaintBucket() {}
+
+  public PaintBucket(ObjectProperty<Color> fillColor) {
+      this.fillColor = fillColor;
+  }
+
+    @Override
+    public void onRelease(GraphicsContext g, MouseEvent e, double size) {
+        double x = e.getX();
+        double y = e.getY();
+
+        //creating WriteableImage with same size as canvas
+        WritableImage image = new WritableImage((int) g.getCanvas().getWidth(), (int) g.getCanvas().getHeight());
+        //capturing current state of canvas
+        g.getCanvas().snapshot(null, image);
+
+        //getting pixel color at mouse position
+        Color targetColor = image.getPixelReader().getColor((int) x, (int) y);
+
+        if (!targetColor.equals(fillColor.getValue())) {
+            floodFill(g, (int) x, (int) y, targetColor, fillColor);
+        }
+    }
+
+    //algorithm to flood fill
+    public static void floodFill(GraphicsContext g, int x, int y, Color targetColor, ObjectProperty<Color> fillColor) {
+      //TODO: making the floodFill algorithm
+    }
 }
