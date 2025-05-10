b7ed9b1b056608bf130cbef03fa65de673b9f28e
schreibtisch1-new-new <g.thieving466@passinbox.com>
2025-05-10 11:44:35 +0200

    PaintBucket: floodFill method implemented; testing if it works

--- src/main/java/org/example/paint/tools/generalTools/PaintBucket.java
+++ src/main/java/org/example/paint/tools/generalTools/PaintBucket.java
@@ -17,26 +17,51 @@ public class PaintBucket implements Tool {
       this.fillColor = fillColor;
   }
 
-    @Override
-    public void onRelease(GraphicsContext g, MouseEvent e, double size) {
-        double x = e.getX();
-        double y = e.getY();
+  //TODO: musing Pipette instead these first lines of code down here
+  @Override
+  public void onRelease(GraphicsContext g, MouseEvent e, double size) {
+      double x = e.getX();
+      double y = e.getY();
 
-        //creating WriteableImage with same size as canvas
-        WritableImage image = new WritableImage((int) g.getCanvas().getWidth(), (int) g.getCanvas().getHeight());
-        //capturing current state of canvas
-        g.getCanvas().snapshot(null, image);
+      //creating WriteableImage with same size as canvas
+      WritableImage image = new WritableImage((int) g.getCanvas().getWidth(), (int) g.getCanvas().getHeight());
+      //capturing current state of canvas
+      g.getCanvas().snapshot(null, image);
 
-        //getting pixel color at mouse position
-        Color targetColor = image.getPixelReader().getColor((int) x, (int) y);
+      //getting pixel color at mouse position
+      Color targetColor = image.getPixelReader().getColor((int) x, (int) y);
 
-        if (!targetColor.equals(fillColor.getValue())) {
-            floodFill(g, (int) x, (int) y, targetColor, fillColor);
-        }
-    }
+      if (!targetColor.equals(fillColor.getValue())) {
+          floodFill(g, (int) x, (int) y, targetColor, fillColor);
+      }
+  }
 
-    //algorithm to flood fill
-    public static void floodFill(GraphicsContext g, int x, int y, Color targetColor, ObjectProperty<Color> fillColor) {
-      //TODO: making the floodFill algorithm
+  //algorithm to flood fill
+  public static void floodFill(GraphicsContext g, int x, int y, Color targetColor, ObjectProperty<Color> fillColor) {
+      //checking starting point being out of bounds
+      if (x < 0 || x >= g.getCanvas().getWidth() || y < 0 || y >= g.getCanvas().getHeight()) {
+          return;
+      }
+
+      //capturing current state of canvas
+      WritableImage image = g.getCanvas().snapshot(null, null);
+
+      //selected neighbor must have the target color
+      if (!image.getPixelReader().getColor(x, y).equals(targetColor)) {
+            return;
+      }
+
+      g.setFill(fillColor.getValue());
+      g.fillRect(x, y, 1, 1);
+
+      //color neighborhood pixels: clockwise
+      floodFill(g, x, y-1, targetColor, fillColor); //south
+      floodFill(g, x-1, y-1, targetColor, fillColor); //southwest
+      floodFill(g, x-1, y, targetColor, fillColor); //west
+      floodFill(g, x-1, y+1, targetColor, fillColor); //northwest
+      floodFill(g, x, y+1, targetColor, fillColor); //north
+      floodFill(g, x+1, y+1, targetColor, fillColor); //northeast
+      floodFill(g, x+1, y, targetColor, fillColor); //east
+      floodFill(g, x+1, y-1, targetColor, fillColor); //southeast
     }
 }
