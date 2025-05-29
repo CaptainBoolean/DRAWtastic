84918bb7c2857f77c73792a09c25a66f6cf7a73d
Paul Moor <paul.alexander.moor@gmail.com>
2025-05-29 14:35:28 +0200

    change to ui

--- src/main/java/org/example/paint/tools/generalTools/RemoveColorFromCanvas.java
+++ src/main/java/org/example/paint/tools/generalTools/RemoveColorFromCanvas.java
@@ -3,4 +3,6 @@ package org.example.paint.tools.generalTools;
 import org.example.paint.tools.Tool;
 
 public class RemoveColorFromCanvas implements Tool {
+
+  //TODO implement click on a color and remove all similar pixels
 }
