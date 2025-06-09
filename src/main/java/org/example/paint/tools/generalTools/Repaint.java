6b9243b21eb19144ea2d08696b9331f64d541dd5
Paul Moor <paul.alexander.moor@gmail.com>
2025-06-09 15:33:44 +0200

    fix repaint

--- src/main/java/org/example/paint/tools/generalTools/Repaint.java
+++ src/main/java/org/example/paint/tools/generalTools/Repaint.java
@@ -10,14 +10,15 @@ import java.util.ArrayList;
 public class Repaint implements Tool, Opaqueable {
 
   @Override
-  public void onRelease(GraphicsContext g, GraphicsContext dg, MouseEvent e, double size) {
+  public void onPress(GraphicsContext g, GraphicsContext dg, MouseEvent e, double size) {
 
     ArrayList<int[]> toRecolor = SelectAreas.floodFillSelected(g, (int) e.getX(), (int) e.getY());  // verwendet bestehende FloodFill
 
     for (int[] pos : toRecolor) {
       int px = pos[0];
       int py = pos[1];
-      dg.fillRect(px, py, 1.1, 1.1); //margin to color in all relevant pixels
+      g.clearRect(px, py, 1, 1);
+      dg.fillRect(px, py, 1, 1);
     }
   }
 }
